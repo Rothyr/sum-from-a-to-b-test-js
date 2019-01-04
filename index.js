34d2119c3de4d077863a1b79e4c18cbf0bcc499a
Dan Amiri-Fear <danielowenfear@gmail.com>
2019-01-04 00:56:29 +0000

    adds completed index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return toN;
+  }
+
+  return toN + sum(fromN, toN - 1);
 }
 
+// console.log(sum(4,9))
 module.exports = sum;
